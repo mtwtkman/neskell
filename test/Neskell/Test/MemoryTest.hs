5b76f1812869a1881f0ae28988fc14433fc79c6b
mtwtkman <924277+mtwtkman@users.noreply.github.com>
2024-01-18 21:19:16 +0900

    testing

--- test/Neskell/Test/MemoryTest.hs
+++ test/Neskell/Test/MemoryTest.hs
@@ -1,12 +1,13 @@
 module Neskell.Test.MemoryTest (tests) where
 
 import Data.Word (Word8)
+import Neskell.Memory (read1)
 import qualified Neskell.Memory as M
-import Neskell.Memory (Memory (Memory), read1)
 import Neskell.Test.Arbitrary ()
 import Test.QuickCheck (NonEmptyList (getNonEmpty))
 import Test.Tasty (TestTree, testGroup)
 import Test.Tasty.QuickCheck (testProperty)
+import Data.Maybe (isNothing)
 
 tests :: TestTree
 tests =
@@ -21,7 +22,12 @@ prop_read1 =
     "read1"
     [ testProperty "should get a value by valid index"
         $ \xs ->
-          let mem = Memory (M.fromList $ getNonEmpty xs :: NonEmptyList [Word8])
+          let mem = M.fromList $ getNonEmpty (xs :: NonEmptyList Word8)
+              idx = fromIntegral (M.length mem) - 1
+           in read1 mem idx == Just (getNonEmpty xs !! (fromIntegral $ toInteger idx :: Int))
+    , testProperty "should Nothing by out of range"
+        $ \xs ->
+          let mem = M.fromList $ getNonEmpty (xs :: NonEmptyList Word8)
               idx = fromIntegral $ M.length mem
-           in read1 mem idx == Just (getNonEmpty xs !! idx)
+           in isNothing (read1 mem idx)
     ]
--- test/tests.hs
+++ test/tests.hs
@@ -5,6 +5,7 @@ import qualified Neskell.Test.CPU.Opcode.LDATest as LDATest
 import qualified Neskell.Test.CPU.Opcode.TAXTest as TAXTest
 import qualified Neskell.Test.CPUTest as CPUTest
 import qualified Neskell.Test.InterpretTest as InterpretTest
+import qualified Neskell.Test.MemoryTest as MemoryTest
 import Test.Tasty (TestTree, defaultMain, testGroup)
 
 main :: IO ()
@@ -19,4 +20,5 @@ tests =
     , BRKTest.tests
     , TAXTest.tests
     , CPUTest.tests
+    , MemoryTest.tests
     ]
